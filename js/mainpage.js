6db58543371fe61b1c2a9eacca5cfd0b20e8531f
WajihaTahir <wajihatahir1994@gmail.com>
2023-12-06 10:14:57 +0100

    done

--- js/mainpage.js
+++ js/mainpage.js
@@ -1,55 +1,47 @@
 function myFunction() {
-    let dots = document.getElementById("dots");
-    let moreText = document.getElementById("more");
-    let btnText = document.getElementById("myBtn");
-  // If the display property of the dots 
-              // to be displayed is already set to 
-              // 'none' (that is hidden) then this 
-              // section of code triggers
-    if (dots.style.display === "none") {
-      moreText.style.display = "none";   // Hide the text between the span elements
-      dots.style.display = "inline";   				// Show the dots after the text
-      btnText.innerHTML = "Read More";  // Change the text on button to 'Read More'
-    } 
-    
-    // If the hidden portion is revealed,
-              // we will change it back to be hidden
-    
-    else {
-  
-      // Show the text between the
-                  // span elements
-      moreText.style.display = "inline";
-  
-                    // Hide the dots after the text
-      dots.style.display = "none";
-      // Change the text on button
-                  // to 'Read Less'
-      btnText.innerHTML = "Read Less";
+  let dots = document.getElementById("dots");
+  let moreText = document.getElementById("more");
+  let btnText = document.getElementById("myBtn");
+  // If the display property of the dots
+  // to be displayed is already set to
+  // 'none' (that is hidden) then this
+  // section of code triggers
+  if (dots.style.display === "none") {
+    moreText.style.display = "none"; // Hide the text between the span elements
+    dots.style.display = "inline"; // Show the dots after the text
+    btnText.innerHTML = "Read More"; // Change the text on button to 'Read More'
   }
+
+  // If the hidden portion is revealed,
+  // we will change it back to be hidden
+  else {
+    // Show the text between the
+    // span elements
+    moreText.style.display = "inline";
+
+    // Hide the dots after the text
+    dots.style.display = "none";
+    // Change the text on button
+    // to 'Read Less'
+    btnText.innerHTML = "Read Less";
   }
-  
-  
-  //changing the size of the text container
-  const div = document.getElementById("textscontainer");
-  document.addEventListener("mouseover", mouseover);
-  document.addEventListener("mouseout", mouseout);
-  
-  function mouseover() {
-    div.style.width = "500px";
-    div.style.height="500px";
-    div.style.marginLeft="480px";
-    div.style.marginBottom='50px';
-   
-  }
-  
-  function mouseout() {
-    div.style.width = "400px";
-    div.style.height="400px";
-    div.style.marginLeft="530px";
-    div.style.marginBottom='50px';
-  }
-  
-  
-  
-  
+}
+
+//changing the size of the text container
+const div = document.getElementById("textscontainer");
+document.addEventListener("mouseover", mouseover);
+document.addEventListener("mouseout", mouseout);
+
+function mouseover() {
+  div.style.width = "500px";
+  div.style.height = "500px";
+  div.style.marginLeft = "480px";
+  div.style.marginBottom = "50px";
+}
+
+function mouseout() {
+  div.style.width = "400px";
+  div.style.height = "400px";
+  div.style.marginLeft = "530px";
+  div.style.marginBottom = "50px";
+}
--- js/search.js
+++ js/search.js
@@ -1,82 +1,89 @@
-const movieSearchBox = document.getElementById("movie-search-box");
-const searchList = document.getElementById("search-list");
-const resultGrid = document.getElementById("result-grid");
-const checkboxes = document.getElementById("checkboxes");
-let allmovies=[];
+const movieSearchBox = document.getElementById("movie-search-box"); // movie-search-box is id of search bar
+const searchList = document.getElementById("search-list"); //a div in which all of the search list elements are such as search list item, thumbnail and info. 
+const resultGrid = document.getElementById("result-grid"); //the grid in which we will display our data.
+const checkboxes = document.getElementById("checkboxes"); //targeted all checkboxes. 
+let allmovies = []; //to be used for checkboxes
 
-
-
-function createListeners()
-{
-  movieSearchBox.addEventListener("keyup", findMovies)
-  const checkElements = checkboxes.querySelectorAll(".type-checkbox");  //added all checboxes to the checkelements which is an object.
-  console.log(checkElements)
-  checkElements.forEach(item => {
-    item.addEventListener("change", ()=> {
+function createListeners() {
+  movieSearchBox.addEventListener("keyup", findMovies);  //allows the specific function to be added when keyup (or something else) happens.
+  const checkElements = checkboxes.querySelectorAll(".type-checkbox"); //added all checboxes to the checkelements which is an object.
+  console.log("lalalala",checkElements);
+  checkElements.forEach((item) => {
+    item.addEventListener("change", () => {
       displayMovieList(allmovies);
-    })
-  })
-
+    });
+  });
 }
 createListeners();
 
 
 
-// load movies from API
-async function loadMovies(searchTerm) {
-  const URL = `https://omdbapi.com/?s=${searchTerm}&apikey=434bb60d`; //calling end points and receiving data in the variable.
-  const res = await fetch(`${URL}`);
-  const data = await res.json(); //the result is being converted to json
-  if (data.Response == "True"){ 
-    displayMovieList(data.Search);  
-    allmovies=data.Search;
-  }
-  else
-  allmovies=[];
-}
-
 function findMovies() {
   let searchTerm = movieSearchBox.value;
+  console.log("this is searchterm", searchTerm);
   if (searchTerm.length > 0) {
     searchList.classList.remove("hide-search-list");
-    loadMovies(searchTerm);
+    loadMovies(searchTerm);   //our actual fetch function
   } else {
     searchList.classList.add("hide-search-list");
   }
 }
 
+
+// load movies from API by live fetch which will run in parallel to other functions because it is async
+async function loadMovies(searchTerm) {   //searchTerm is coming from the findMovies function. 
+  try{
+  const URL = `https://omdbapi.com/?s=${searchTerm}&apikey=434bb60d`; //calling end points and receiving data in the variable.
+  const res = await fetch(`${URL}`);
+  const data = await res.json(); //the result is being converted to json
+  if (data.Response == "True") {
+    console.log("this is data", data.Response);
+    displayMovieList(data.Search);
+    console.log("data.Search", data.Search) //this will give me array of objects with all movies with that name
+    allmovies = data.Search;   //if the response is coming out to be true, the allmovies array takes on the search value. 
+  } else allmovies = [];
+}
+catch(e)
+{
+  console.log("There might be error, please check",);
+}
+}
+
+
+
+
+
+
 function displayMovieList(movieData) {
-  searchList.innerHTML = "";  //emptying the searchList each time
-  const checkElements = checkboxes.querySelectorAll(".type-checkbox");  //added all checboxes to the checkelements which is an object.
-  
-  let checked = "all"; 
-  checkElements.forEach((item)=>{      //pushed all those elements to form an array of checkboxes using anon function.
+  searchList.innerHTML = ""; //emptying the searchList each time
+  const checkElements = checkboxes.querySelectorAll(".type-checkbox"); //added all checboxes to the checkelements which is an object.
+
+  let checked = "all";  //when we load the page, all is selected
+  checkElements.forEach((item) => {
+    //pushed all those elements to form an array of checkboxes using anon function.
     // checks.push(item);
-    if(item.checked)
-    {
-    checked=item.value;
+    if (item.checked) {
+      checked = item.value; //here is is getting separate value's values based on whether the user check the boxes or not.
+      console.log("see it is checked", checked)  //the checked will display series or movie based on what is checked. 
     }
-  })
-  let movies = [];  
-  console.log(checked); //now we want another array which will check the checking of those checkboxes, this is the array which we will be using ahead in our list. 
-  if (checked==="all"){
-    
-  movies=movieData;
+  });
+  let movies = [];
+  // console.log(checked); //now we want another array which will check the checking of those checkboxes, this is the array which we will be using ahead in our list.
+  if (checked === "all") {
+    movies = movieData;
+  } else {
+    movies = movieData.filter((item) => {
+      console.log("item", item.Type);
+      return item.Type === checked; //(if it is checked) will return true or false for both checkboxes based on their checking condition. 
+    });
   }
-  else
-  {
-    
-    movies=movieData.filter((item)=>{
-    return item.Type===checked;
-    })
-  }
-  
+
   for (let idx = 0; idx < movies.length; idx++) {
     let movieListItem = document.createElement("div");
     movieListItem.dataset.id = movies[idx].imdbID; // setting movie id in  data-id
     movieListItem.classList.add("search-list-item");
     if (movies[idx].Poster != "N/A") moviePoster = movies[idx].Poster;
-    else moviePoster = "Imagenotfound.jpeg";
+    else moviePoster = "../Imagenotfound.jpeg";
     const thumbnail1 = document.createElement("div");
     thumbnail1.classList.add("search-item-thumbnail");
     const image = document.createElement("img");
@@ -96,6 +103,9 @@ function displayMovieList(movieData) {
   loadMovieDetails();
 }
 
+
+
+
 function loadMovieDetails() {
   const searchListMovies = searchList.querySelectorAll(".search-list-item");
   searchListMovies.forEach((movie) => {
@@ -113,13 +123,17 @@ function loadMovieDetails() {
   });
 }
 
+
+
+
+
 function displayMovieDetails(details) {
   resultGrid.innerHTML = `
   <div class="flip-card">
   <div class="flip-card-inner">
     <div class = "movie-poster">
         <img src = "${
-          details.Poster != "N/A" ? details.Poster : "image_not_found.png"
+          details.Poster != "N/A" ? details.Poster : "../Imagenotfound.jpeg"
         }" alt = "movie poster">
     </div>
     <div class="flip-card-back">
@@ -145,6 +159,4 @@ function displayMovieDetails(details) {
         }</p>
     </div>
     `;
-};
-
-
+}
